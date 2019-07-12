ccf5b5853fa698adb28a3f2fed08aaeaaa31dcb9
nguyenhung9x2018 <nguyenhung9x2018@gmail.com>
2019-07-12 09:49:35 +0700

    Add version for ZenOS

--- config/ota.mk
+++ config/ota.mk
@@ -1,14 +1,14 @@
 ifneq ($(IS_GENERIC_SYSTEM_IMAGE), true)
-ifeq ($(CUSTOM_BUILD_TYPE), OFFICIAL)
+ifeq ($(ZEN_BUILD_TYPE), OFFICIAL)
 
 ifeq ($(IS_GO_VERSION), true)
-CUSTOM_OTA_VERSION_CODE := pie_go
+ZEN_OTA_VERSION_CODE := pie_go
 else
-CUSTOM_OTA_VERSION_CODE := pie
+ZEN_OTA_VERSION_CODE := pie
 endif
 
 CUSTOM_PROPERTIES += \
-    org.zen.ota.version_code=$(CUSTOM_OTA_VERSION_CODE) \
+    org.zen.ota.version_code=$(ZEN_OTA_VERSION_CODE) \
     sys.ota.disable_uncrypt=1
 
 PRODUCT_PACKAGES += \
