f85306379e62c525513a9dab2a24541f2a0fb3b6
nguyenhung9x2018 <nguyenhung9x2018@gmail.com>
2019-07-12 17:56:53 +0700

    Add list devices support OFFCIAL

--- config/branding.mk
+++ config/branding.mk
@@ -5,24 +5,35 @@ ZEN_DATE_MONTH := $(shell date -u +%m)
 ZEN_DATE_DAY := $(shell date -u +%d)
 ZEN_DATE_HOUR := $(shell date -u +%H)
 ZEN_DATE_MINUTE := $(shell date -u +%M)
-ZEN_BUILD_DATE_UTC := $(shell date -d '$(ZEN_DATE_YEAR)-$(ZEN_DATE_MONTH)-$(ZEN_DATE_DAY) $(ZEN_DATE_HOUR):$(ZEN_DATE_MINUTE) UTC' +%s)
-ZEN_BUILD_DATE := $(ZEN_DATE_YEAR)$(ZEN_DATE_MONTH)$(ZEN_DATE_DAY)-$(ZEN_DATE_HOUR)$(ZEN_DATE_MINUTE)
+ZEN_MAINTAINER := None
 ZEN_PLATFORM_VERSION := 9.0
 ZenOS_VERSION := BETA
 
 TARGET_PRODUCT_SHORT := $(subst zen_,,$(ZEN_BUILD))
 
-ifeq ($(IS_GO_VERSION), true)
-ZEN_VERSION := ZenOS_go_$(ZEN_BUILD)-$(ZEN_PLATFORM_VERSION)-$(ZEN_BUILD_DATE)-$(ZEN_BUILD_TYPE)
-ZEN_FINGERPRINT := ZenOS_go/$(ZEN_PLATFORM_VERSION)/$(ZEN_PRODUCT_SHORT)/$(ZEN_BUILD_DATE)
-else
-ZEN_VERSION := ZenOS_$(ZEN_BUILD)-$(ZEN_VERSION)-$(ZEN_BUILD_DATE)-$(ZEN_BUILD_TYPE)
-ROM_FINGERPRINT := ZenOS/$(ZEN_PLATFORM_VERSION)/$(ZEN_PRODUCT_SHORT)/$(ZEN_BUILD_DATE)
-endif
+CURRENT_DEVICE=$(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3)
+
+ifeq ($(ZEN_OFFICIAL),true)
+   LIST = $(shell curl -s https://raw.githubusercontent.com/zenos-made-by-zentalk/android_vendor_zen/pie/zen.devices)
+   FOUND_DEVICE = $(filter $(CURRENT_DEVICE), $(LIST))
+    ifeq ($(FOUND_DEVICE),$(CURRENT_DEVICE))
+      IS_OFFICIAL=true
+      ZEN_BUILD_TYPE := OFFICIAL
+    else
+      ZEN_BUILD_TYPE := UNOFFICIAL
+    endif
+endif   
+
+ZEN_BUILD_DATE_UTC := $(shell date -d '$(ZEN_DATE_YEAR)-$(ZEN_DATE_MONTH)-$(ZEN_DATE_DAY) $(ZEN_DATE_HOUR):$(ZEN_DATE_MINUTE) UTC' +%s)
+ZEN_NAME := ZenOS-$(ZenOS_VERSION)-$(CURRENT_DEVICE)-$(ZEN_BUILD_TYPE)-$(ZEN_BUILD_DATE)
+ZEN_BUILD_DATE := $(ZEN_DATE_YEAR)$(ZEN_DATE_MONTH)$(ZEN_DATE_DAY)-$(ZEN_DATE_HOUR)$(ZEN_DATE_MINUTE)
+ZEN_VERSION := ZenOS-$(ZEN_BUILD)-$(ZEN_PLATFORM_VERSION)-$(ZEN_BUILD_DATE)-$(ZEN_BUILD_TYPE)
+ZEN_FINGERPRINT := ZenOS/$(ZEN_PLATFORM_VERSION)/$(ZEN_PRODUCT_SHORT)/$(ZEN_BUILD_DATE)
 
 ZEN_PROPERTIES := \
     org.zen.version=$(ZenOS_VERSION) \
     org.zen.build_date=$(ZEN_BUILD_DATE) \
     org.zen.build_date_utc=$(ZEN_BUILD_DATE_UTC) \
     org.zen.build_type=$(ZEN_BUILD_TYPE) \
-    org.zen.fingerprint=$(ZEN_FINGERPRINT)
+    org.zen.fingerprint=$(ZEN_FINGERPRINT) \
+    org.zen.maintainer=$(ZEN_MAINTAINER)
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for device in $(python vendor/zen/tools/get_official_devices.py)
+for device in $(curl -s https://raw.githubusercontent.com/zenos-made-by-zentalk/android_vendor_zen/pie/zen.devices)
 do
 for var in eng user userdebug; do
 add_lunch_combo zen_$device-$var
